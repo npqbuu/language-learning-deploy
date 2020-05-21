061621775acb18da176dbfdfedeb99fec4a25f6d
Buu NGUYEN <17428690+npqbuu@users.noreply.github.com>
2020-05-21 14:07:41 +0200

    update GUI

--- app/views.py
+++ app/views.py
@@ -41,6 +41,11 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+
+    return render_template("about.html")
+
 @app.route('/dashboard')
 @login_required
 def dashboard():
